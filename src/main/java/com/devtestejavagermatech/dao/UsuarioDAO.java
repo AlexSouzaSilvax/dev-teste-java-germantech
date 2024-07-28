968629e201691e71511fac3482de80ca8f557013
Alex Souza da Silva <alexsouzasilvax@gmail.com>
2024-07-28 18:07:32 -0300

    implements dao usuario

---
+++ src/main/java/com/devtestejavagermatech/dao/UsuarioDAO.java
@@ -0,0 +1,35 @@
+package com.devtestejavagermatech.dao;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.devtestejavagermatech.model.Usuario;
+
+public class UsuarioDAO {
+
+    public void create(Usuario usuario) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'create'");
+    }
+
+    public List<Usuario> read() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'listar'");
+    }
+
+    public void update(Usuario usuario) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    public void delete(String string) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    }
+
+    public Optional<Usuario> buscarUsuarioPorCpf(String string) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'buscarUsuarioPorCpf'");
+    }
+
+}
