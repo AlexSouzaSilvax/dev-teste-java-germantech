be87a77d503d08f1913e6f23468f2a2620ef7b04
Alex Souza da Silva <alexsouzasilvax@gmail.com>
2024-07-28 18:31:22 -0300

    add create

--- src/main/java/com/devtestejavagermatech/dao/UsuarioDAO.java
+++ src/main/java/com/devtestejavagermatech/dao/UsuarioDAO.java
@@ -1,15 +1,39 @@
 package com.devtestejavagermatech.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
 import com.devtestejavagermatech.model.Usuario;
+import com.devtestejavagermatech.util.ConexaoDB;
+import com.devtestejavagermatech.util.exception.ErroSistema;
 
 public class UsuarioDAO {
 
-    public void create(Usuario usuario) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'create'");
+    public void create(Usuario usuario) throws ErroSistema {
+        try {
+            Connection conexao = ConexaoDB.getConexao();
+            PreparedStatement ps;
+            if (usuario.getId() == null) {
+                ps = conexao.prepareStatement(
+                        "INSERT INTO `usuario_` (`nome`,`telefone`,`email`,`cpf`,`senha`) VALUES (?,?,?,?,?)");
+            } else {
+                ps = conexao
+                        .prepareStatement("update usuario_ set nome=?, telefone=?, email=?, cpf=?, senha=? where id=?");
+                ps.setString(6, usuario.getId().toString());
+            }
+            ps.setString(1, usuario.getNome());
+            ps.setString(2, usuario.getTelefone());
+            ps.setString(3, usuario.getEmail());
+            ps.setString(4, usuario.getCpf());
+            ps.setString(5, usuario.getSenha());
+            ps.execute();
+            ConexaoDB.fecharConexao();
+        } catch (SQLException ex) {
+            throw new ErroSistema("Erro ao tentar salvar!", ex);
+        }
     }
 
     public List<Usuario> read() {
