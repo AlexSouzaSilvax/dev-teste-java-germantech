40ee66bbab6106a6811db99aa0e960c85d8a5b74
Alex Souza da Silva <alexsouzasilvax@gmail.com>
2024-07-28 18:07:00 -0300

    add model usuario

---
+++ src/main/java/com/devtestejavagermatech/model/Usuario.java
@@ -0,0 +1,71 @@
+package com.devtestejavagermatech.model;
+
+import java.util.UUID;
+
+public class Usuario {
+
+    private UUID id;
+    private String nome;
+    private String telefone;
+    private String email;
+    private String cpf;
+    private String senha;
+
+    public Usuario(UUID id, String nome, String telefone, String email, String cpf, String senha) {
+        this.id = id;
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+        this.cpf = cpf;
+        this.senha = senha;
+    }
+
+    public UUID getId() {
+        return this.id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return this.telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return this.cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getSenha() {
+        return this.senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+}
