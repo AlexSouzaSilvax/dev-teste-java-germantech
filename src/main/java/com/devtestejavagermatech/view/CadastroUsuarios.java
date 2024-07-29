4f749f98476622e02ba4157923b0506ee3054bf1
Alex Souza da Silva <alexsouzasilvax@gmail.com>
2024-07-29 15:42:04 -0300

    melhoria codigo, ui

--- src/main/java/com/devtestejavagermatech/view/CadastroUsuarios.java
+++ src/main/java/com/devtestejavagermatech/view/CadastroUsuarios.java
@@ -6,7 +6,6 @@ import javax.swing.table.TableCellRenderer;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
 import java.util.List;
@@ -26,10 +25,8 @@ public class CadastroUsuarios extends JFrame {
     private DefaultTableModel tableModel;
 
     private List<Usuario> listaUsuarios = new ArrayList<>();
-
-    UsuarioController usuarioController = new UsuarioController();
-
-    Usuario usuarioSelecionado = new Usuario();
+    private UsuarioController usuarioController = new UsuarioController();
+    private Usuario usuarioSelecionado = new Usuario();
 
     public CadastroUsuarios() throws ErroSistema {
         initialize();
@@ -42,208 +39,166 @@ public class CadastroUsuarios extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
 
-        JPanel formPanel = new JPanel();
-        formPanel.setLayout(new GridLayout(5, 2, 10, 10)); // 5 linhas, 2 colunas, 10px de espaço entre componentes
-        formPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10)); // Bordas internas
+        JPanel formPanel = createFormPanel();
+        JPanel buttonPanel = createButtonPanel();
+        JPanel topPanel = new JPanel(new BorderLayout());
+        topPanel.add(formPanel, BorderLayout.CENTER);
+        topPanel.add(buttonPanel, BorderLayout.SOUTH);
+
+        add(topPanel, BorderLayout.NORTH);
+        add(createTableScrollPane(), BorderLayout.CENTER);
+
+        setLocationRelativeTo(null);
+    }
+
+    private JPanel createFormPanel() {
+        JPanel formPanel = new JPanel(new GridLayout(3, 4, 10, 10));
+        formPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+        nomeField = new JTextField();
+        telefoneField = new JFormattedTextField(usuarioController.createFormatter("(##) #####-####"));
+        emailField = new JTextField();
+        cpfField = new JFormattedTextField(usuarioController.createFormatter("###.###.###-##"));
+        senhaField = new JPasswordField();
 
         formPanel.add(new JLabel("Nome:"));
-        nomeField = new JTextField();
         formPanel.add(nomeField);
-
         formPanel.add(new JLabel("Telefone:"));
-        telefoneField = new JFormattedTextField(usuarioController.createFormatter("(##) #####-####"));
         formPanel.add(telefoneField);
-
         formPanel.add(new JLabel("Email:"));
-        emailField = new JTextField();
         formPanel.add(emailField);
-
         formPanel.add(new JLabel("CPF:"));
-        cpfField = new JFormattedTextField(usuarioController.createFormatter("###.###.###-##"));
         formPanel.add(cpfField);
-
         formPanel.add(new JLabel("Senha:"));
-        senhaField = new JPasswordField();
         formPanel.add(senhaField);
 
-        JPanel buttonPanel = new JPanel();
-        buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10)); // Alinhamento dos botões à direita
+        return formPanel;
+    }
+
+    private JPanel createButtonPanel() {
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 10));
 
         JButton novoButton = new JButton("Novo");
-        novoButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                limpaForm();
-            }
-        });
+        novoButton.addActionListener(e -> limpaForm());
         buttonPanel.add(novoButton);
 
         JButton addButton = new JButton("Salvar");
-
-        addButton.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    createUsuario();
-                } catch (ErroSistema e1) {
-                    e1.printStackTrace();
-                }
-                limpaForm();
+        addButton.addActionListener(e -> {
+            try {
+                createUsuario();
+            } catch (ErroSistema e1) {
+                e1.printStackTrace();
             }
+            limpaForm();
         });
         buttonPanel.add(addButton);
 
         JButton searchButton = new JButton("Buscar");
-        searchButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    buscarUsuarios();
-                } catch (ErroSistema e1) {
-                    e1.printStackTrace();
-                }
-                limpaForm();
+        searchButton.addActionListener(e -> {
+            try {
+                buscarUsuarios();
+            } catch (ErroSistema e1) {
+                e1.printStackTrace();
             }
+            limpaForm();
         });
         buttonPanel.add(searchButton);
 
         JButton listarButton = new JButton("Listar");
-        listarButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    readUsuarios();
-                } catch (ErroSistema e1) {
-                    e1.printStackTrace();
-                }
-                limpaForm();
+        listarButton.addActionListener(e -> {
+            try {
+                readUsuarios();
+            } catch (ErroSistema e1) {
+                e1.printStackTrace();
             }
+            limpaForm();
         });
         buttonPanel.add(listarButton);
 
         JButton atualizaButton = new JButton("Atualizar");
         atualizaButton.setVisible(false);
-
-        atualizaButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                atualizaButton.setVisible(false);
-                System.out.println(e);
-                try {
-                    atualizaUsuario();
-                } catch (ErroSistema e1) {
-                    e1.printStackTrace();
-                }
-                limpaForm();
+        atualizaButton.addActionListener(e -> {
+            try {
+                atualizaUsuario();
+            } catch (ErroSistema e1) {
+                e1.printStackTrace();
             }
+            limpaForm();
+            atualizaButton.setVisible(false);
         });
         buttonPanel.add(atualizaButton);
 
         JButton deleteButton = new JButton("Excluir");
-        deleteButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    excluirUsuario();
-                } catch (ErroSistema e1) {
-                    e1.printStackTrace();
-                }
-                limpaForm();
-            }
-        });
         deleteButton.setVisible(false);
+        deleteButton.addActionListener(e -> {
+            try {
+                excluirUsuario();
+            } catch (ErroSistema e1) {
+                e1.printStackTrace();
+            }
+            limpaForm();
+        });
         buttonPanel.add(deleteButton);
 
-        JPanel topPanel = new JPanel();
-        topPanel.setLayout(new BorderLayout());
-        topPanel.add(formPanel, BorderLayout.CENTER);
-        topPanel.add(buttonPanel, BorderLayout.SOUTH);
+        return buttonPanel;
+    }
 
-        add(topPanel, BorderLayout.NORTH);
-
+    private JScrollPane createTableScrollPane() {
         tableModel = new DefaultTableModel(new String[] { "ID", "Nome", "Telefone", "Email", "CPF" }, 0);
-
         table = new JTable(tableModel);
+        table.setRowHeight(30);
 
         table.addFocusListener(new FocusAdapter() {
+            @Override
             public void focusGained(FocusEvent e) {
                 int row = table.getSelectedRow();
                 if (row >= 0) {
-                    deleteButton.setVisible(true);
                     atualizaUsuarioForm(listaUsuarios.get(row));
                 }
             }
         });
 
-        add(new JScrollPane(table), BorderLayout.CENTER);
+        return new JScrollPane(table);
     }
 
     private void readUsuarios() throws ErroSistema {
-
         tableModel.setRowCount(0);
-
-        for (Usuario u : listaUsuarios = usuarioController.read()) {
+        listaUsuarios = usuarioController.read();
+        for (Usuario u : listaUsuarios) {
             tableModel.addRow(new Object[] {
-                    u.getId(),
-                    u.getNome(),
-                    u.getTelefone(),
-                    u.getEmail(),
-                    u.getCpf()
+                u.getId(), u.getNome(), u.getTelefone(), u.getEmail(), u.getCpf()
             });
         }
     }
 
     private void createUsuario() throws ErroSistema {
-
-        tableModel.setRowCount(0);
-
         if (validarCampos()) {
-            usuarioController.create(new Usuario(nomeField.getText(), telefoneField.getText(),
-                    emailField.getText(), cpfField.getText(), new String(senhaField.getPassword())));
+            usuarioController.create(new Usuario(nomeField.getText(), telefoneField.getText(), emailField.getText(),
+                cpfField.getText(), new String(senhaField.getPassword())));
             readUsuarios();
         }
-
     }
 
     private void atualizaUsuario() throws ErroSistema {
-
         if (validarCampos()) {
-
-            usuarioController
-                    .update(new Usuario(usuarioSelecionado.getId(), nomeField.getText(), telefoneField.getText(),
-                            emailField.getText(), cpfField.getText(), new String(senhaField.getPassword())));
-
+            usuarioController.update(new Usuario(usuarioSelecionado.getId(), nomeField.getText(), telefoneField.getText(),
+                emailField.getText(), cpfField.getText(), new String(senhaField.getPassword())));
             readUsuarios();
         }
     }
 
     private void excluirUsuario() throws ErroSistema {
-
-        tableModel.setRowCount(0);
-
-        String cpf = cpfField.getText();
-
-        usuarioController.delete(cpf);
-
+        usuarioController.delete(cpfField.getText());
         readUsuarios();
     }
 
     private void buscarUsuarios() throws ErroSistema {
-        String nome = nomeField.getText();
-        String email = emailField.getText();
-        String cpf = cpfField.getText();
         tableModel.setRowCount(0);
-        for (Usuario u : usuarioController.buscarByNomeOuEmailOuCpf(nome, email, cpf)) {
+        for (Usuario u : usuarioController.buscarByNomeOuEmailOuCpf(nomeField.getText(), emailField.getText(), cpfField.getText())) {
             tableModel.addRow(new Object[] {
-                    u.getId(),
-                    u.getNome(),
-                    u.getTelefone(),
-                    u.getEmail(),
-                    u.getCpf()
+                u.getId(), u.getNome(), u.getTelefone(), u.getEmail(), u.getCpf()
             });
         }
-
     }
 
     private void atualizaUsuarioForm(Usuario usuarioSelecionado) {
@@ -253,7 +208,6 @@ public class CadastroUsuarios extends JFrame {
         emailField.setText(usuarioSelecionado.getEmail());
         cpfField.setText(usuarioSelecionado.getCpf());
         senhaField.setText(usuarioSelecionado.getSenha());
-
     }
 
     private boolean validarCampos() {
@@ -263,38 +217,27 @@ public class CadastroUsuarios extends JFrame {
         String email = emailField.getText();
         String senha = new String(senhaField.getPassword());
 
-        if (nome.isEmpty() || nome.equalsIgnoreCase(" ") || nome.equals(null)) {
+        if (nome.isEmpty()) {
             JOptionPane.showMessageDialog(this, "Nome é obrigatório!");
             return false;
         }
-
-        if (cpf.isEmpty() || cpf.equals("   .   .   -  ") || cpf.equals(null)) {
+        if (cpf.isEmpty()) {
             JOptionPane.showMessageDialog(this, "CPF é obrigatório!");
             return false;
         }
-
         if (!usuarioController.isValidCPF(cpf)) {
             JOptionPane.showMessageDialog(this, "CPF inválido!");
             return false;
         }
-
-        if (!telefone.equals("(  )      -    ") && usuarioController.verificaNTelefone(telefone)) {
-            if (!usuarioController.isValidTelefone(telefone)) {
-                JOptionPane.showMessageDialog(this, "Telefone inválido!");
-                return false;
-            }
-        } else {
-            telefoneField.setText("");
+        if (!telefone.trim().isEmpty() && !usuarioController.isValidTelefone(telefone)) {
+            JOptionPane.showMessageDialog(this, "Telefone inválido!");
+            return false;
         }
-
-        if (!email.isEmpty() || !email.equals("")) {
-            if (!usuarioController.isValidEmail(email)) {
-                JOptionPane.showMessageDialog(this, "Email inválido!");
-                return false;
-            }
+        if (!email.trim().isEmpty() && !usuarioController.isValidEmail(email)) {
+            JOptionPane.showMessageDialog(this, "Email inválido!");
+            return false;
         }
-
-        if (senha.isEmpty() || senha == null) {
+        if (senha.isEmpty()) {
             JOptionPane.showMessageDialog(this, "Senha é obrigatória!");
             return false;
         }
@@ -310,23 +253,18 @@ public class CadastroUsuarios extends JFrame {
     }
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                CadastroUsuarios c;
-                try {
-                    c = new CadastroUsuarios();
-                    c.setVisible(true);
-                    c.setLocationRelativeTo(null);
-                } catch (ErroSistema e) {
-                    e.printStackTrace();
-                }
+        SwingUtilities.invokeLater(() -> {
+            try {
+                CadastroUsuarios frame = new CadastroUsuarios();
+                frame.setVisible(true);
+            } catch (ErroSistema e) {
+                e.printStackTrace();
             }
         });
     }
 }
 
 class ButtonRenderer extends JButton implements TableCellRenderer {
-
     public ButtonRenderer() {
         setOpaque(true);
         setText("Edit");
@@ -347,7 +285,6 @@ class ButtonRenderer extends JButton implements TableCellRenderer {
 }
 
 class ButtonEditor extends DefaultCellEditor {
-
     private final JButton button;
     private final List<Usuario> users;
     private Usuario selectedUser;
@@ -361,18 +298,12 @@ class ButtonEditor extends DefaultCellEditor {
         button.setOpaque(true);
         button.setText("Edit");
 
-        button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                fireEditingStopped(); // Stop editing and commit changes
-
-            }
-        });
+        button.addActionListener(e -> fireEditingStopped());
     }
 
     @Override
     public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column) {
-        selectedUser = users.get(row); // Get the user object for the current row
+        selectedUser = users.get(row);
         button.setText("Edit");
         isPushed = true;
         return button;
